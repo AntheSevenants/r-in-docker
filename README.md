2b51c28fe8b4ec95da29806b107dc8f6753c385c
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2024-10-24 20:55:21 +0200

    Add script and README

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # r-in-docker
-A script to spin up an R project that runs in Docker
+A script to spin up an R (Studio) project that runs in Docker
+
+Since I've had to change computers at work more often than I'd like (looking at you, HP), I built a workflow around running R and its packages within a Docker container. The benefit of this is that my environments are fully reproducible, and can be spun up within minutes, including dependencies.
+
+The downside is that it takes a bit of prep work to get to this point. This repository automates that work in a single script.
+
+## Assumptions
+
+* UNIX-like operating system (real or virtualised)
+* Docker is installed
+* Current user is in the `docker` group
+
+## How to use
+
+### Setup
+
+1. `git clone https://github.com/AntheSevenants/r-in-docker.git`
+2. `cd r-in-docker`
+3. `chmod +x r-in-docker.sh`
+4. `sudo cp r-in-docker.sh /usr/local/bin/r-in-docker.sh`
+
+### Use
+
+1. Navigate to the directory where you want the project folder to be created.
+2. Run `r-in-docker.sh`. If you supply an argument, that argument will be used as the project name. Else, the script will ask for the project name.
+3. The project directory and files will be created automatically. All that remains to do is to run `docker compose up` in the project directory.
+
+## Notice
+
+- You will have to rebuild your images if you add dependencies. You can do this by running `sh .docker/build.sh` from within the project directory. Make sure to snapshot your dependencies with `renv:snapshot()` from within R. This will update the lockfile (`renv.lock`)
+- Docker images will be generated with the 'anthesevenants/' prefix and my git credentials. That's me, after all. If you want to change this, feel free to fork this repository.
---
+++ r-in-docker.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "Enter project name: (prefer A-za-z, no spaces)"
+    read project_name
+  else
+    project_name=$1
+    echo "Using '$project_name' as project name"
+fi
+
+
+project_root="$(pwd)/$project_name/"
+
+# Make Docker-safe
+project_name_image=${project_name,,}
+echo "Docker image name will be '$project_name_image'"
+
+mkdir $project_root
+
+#
+# renv.lock
+#
+echo "{}" > $project_root/renv.lock
+
+#
+# Dockerfile.base
+#
+dockerfile_base="# I don't really care about storage, I care about reusability
+# So, the base image will be the rocker/rstudio image
+# This already contains R and R studio, which is mostly what we'll need
+FROM rocker/rstudio:4
+
+# First, I copy all dependencies into the container
+COPY renv.lock renv.lock
+
+# Install some linux libraries that R packages need
+RUN apt-get update && apt-get install -y libcurl4-openssl-dev libssl-dev libxml2-dev libodbc1 libxt6
+
+# Then, I install 'renv', and then restore all dependencies
+# renv will download and install everything as needed
+RUN R -q -e \"install.packages('renv', repos = c(CRAN = 'https://cloud.r-project.org'))\"
+RUN R -q -e \"renv::restore()\"
+
+
+# This image can now be used to launch an R Studio environment"
+echo "$dockerfile_base" > $project_root/Dockerfile.base
+
+#
+# Dockerfile.rstudio
+#
+dockerfile_rstudio="FROM anthesevenants/$project_name_image:base
+# We start from the R Studio base
+
+RUN apt-get install -y lsof
+
+# Let's set the working directory correctly (also for the R environment itself)
+RUN echo \"setwd(\"/home/rstudio/$project_name/\")\" > ~/../home/rstudio/.Rprofile
+RUN mkdir -p \"/home/rstudio/.local/share/rstudio/projects_settings/\"
+RUN mkdir -p \"/root/.local/share/rstudio/projects_settings/\"
+RUN chmod -R 777 \"/home/rstudio/.local/\"
+RUN chmod -R 777 \"/root/.local/\"
+RUN echo \"/home/rstudio/$project_name/$project_name.Rproj\" > \"/home/rstudio/.local/share/rstudio/projects_settings/last-project-path\"
+RUN echo \"/home/rstudio/$project_name/$project_name.Rproj\" > \"/root/.local/share/rstudio/projects_settings/last-project-path\""
+
+echo "$dockerfile_rstudio" > $project_root/Dockerfile.rstudio
+
+#
+# docker-compose.yml
+#
+docker_compose_yml="version: \"3\"
+
+services:
+  rstudio:
+    image: \"anthesevenants/$project_name_image:rstudio\"
+    ports: 
+      - "8787:8787"
+      - "8788:8788"
+    environment:
+      DISABLE_AUTH: true
+    volumes:
+      - .:/home/rstudio/$project_name/:Z"
+echo "$docker_compose_yml" > $project_root/docker-compose.yml
+
+# projectname.Rproj
+project_name_Rproj="Version: 1.0
+
+RestoreWorkspace: Default
+SaveWorkspace: Default
+AlwaysSaveHistory: Default
+
+EnableCodeIndexing: Yes
+UseSpacesForTab: Yes
+NumSpacesForTab: 2
+Encoding: UTF-8
+
+RnwWeave: Sweave
+LaTeX: pdfLaTeX"
+
+echo "$project_name_Rproj" > "$project_root/$project_name.Rproj"
+
+#
+# .docker/
+#
+mkdir "$project_root/.docker/"
+
+#
+# .docker/build.sh
+#
+build_sh="docker build \\
+    -f Dockerfile.base \\
+    -t anthesevenants/$project_name_image:base .
+
+docker build \\
+    -f Dockerfile.rstudio \\
+    -t anthesevenants/$project_name_image:rstudio ."
+build_sh_path=$project_root/.docker/build.sh
+echo "$build_sh" > $build_sh_path
+
+echo "Building Docker images"
+chmod +x $build_sh_path
+cd $project_root && sh $build_sh_path
+cd $project_root && git init
+cd $project_root && git config user.name "Anthe Sevenants"
+cd $project_root && git config user.email "anthe.sevenants@kuleuven.be"
+
+echo "All done!"
+echo "Navigate to '$project_root' and run 'docker compose up'"
+echo "- Anthe signing off... 2024-10-24, 20:46"
